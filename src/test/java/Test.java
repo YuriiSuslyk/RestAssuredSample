f2d74e2c3296ad2e461d16c7b2b18922b67e08a6
YuriiSuslyk <yurii.suslyk@gmail.com>
2019-10-25 21:36:09 +0300

    fhhggj

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,18 +3,17 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.lessThan;
 
-public class RestAssuredSpecGet {
+public class Test {
 
     RequestSpecification requestSpec = new RequestSpecBuilder()
             .setBaseUri("https://reqres.in/")
-            .setContentType(ContentType.JSON) // same as .header("Content-Type", "application/json")
+            .setContentType(ContentType.JSON)
             .setBasePath("/api/users")
             .build();
 
@@ -23,32 +22,19 @@ public class RestAssuredSpecGet {
             .expectResponseTime(lessThan(5000L))
             .build();
 
-    @Test
-    public void restGet() {
-        given()
-                .spec(requestSpec)
-                .pathParam("id", 2)
-                .when()
-                .get()
-                .then()
-                .spec(responseSpec)
-                .body("email" :"michael.lawson@reqres.in"), equalTo("Michael"))
-                .and
-                .body("Lawson", equalTo("Weaver"));
-    }
 
-    @Test
+
     public void restGet2() {
         given()
                 .spec(requestSpec)
-                .param("page",2)
+                .pathParam("id", 3)
                 .when()
                 .get()
                 .then()
                 .spec(responseSpec)
                 .and()
-                .body("data.first_name", equalTo("Emma"))
+                .body("email\" :\"michael.lawson@reqres.in", equalTo("Emma"))
                 .and()
-                .body("data.last_name", equalTo("Wong"));
+                .body("Michael", equalTo("Wong"));
     }
-}
+    }
